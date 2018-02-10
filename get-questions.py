a2852ecba194242ee3ca60ef20d4df93255c2654
Lars Vögtlin <lvoegtlin@gmail.com>
2018-02-10 23:47:25 +0100

    script adaped to the new questions

--- get-questions.py
+++ get-questions.py
@@ -6,15 +6,15 @@ import random
 conn = sqlite3.connect('questions.db')
 cursor = conn.cursor()
 
-def get_question_and_save_into_db( category ):
-    questions_path = "quizduellFragen.csv"
+def get_question_and_save_into_db():
+    questions_path = "quizQuestions.txt"
 
     questions_file = open(questions_path, 'r')
 
     for question in questions_file:
         question_list = question.split(";")
-        cursor.execute('INSERT INTO downloaded_question(id, answer, question, value) VALUES (?, ?, ?, ?)',
-                (question['id'], question['answer'], question['question'], question['value']))
+        cursor.execute('INSERT INTO downloaded_question(answer, question, value) VALUES (?, ?, ?)',
+                (question_list[1], question_list[0], random_value()))
 
     conn.commit()
 
@@ -22,4 +22,6 @@ def get_question_and_save_into_db( category ):
 def random_value():
     return 100 * random.randint(1,3)
 
+
+get_question_and_save_into_db()
 
