4a0dc61d69286941753d120e8b020ece2d5c6b8f
Lars Vögtlin <lvoegtlin@gmail.com>
2018-02-10 23:30:55 +0100

    start with german question

--- get-questions.py
+++ get-questions.py
@@ -1,22 +1,25 @@
-#we get the questions from http://www.jservice.io/ and load it into the question db
-
 import sqlite3
 import requests
 import json
+import random
+
 conn = sqlite3.connect('questions.db')
 cursor = conn.cursor()
 
 def get_question_and_save_into_db( category ):
-    r = requests.get('http://www.jservice.io/api/category?id='+category)
-    questions = r.json()['clues']
+    questions_path = "quizduellFragen.csv"
 
-    for question in questions:
+    questions_file = open(questions_path, 'r')
+
+    for question in questions_file:
+        question_list = question.split(";")
         cursor.execute('INSERT INTO downloaded_question(id, answer, question, value) VALUES (?, ?, ?, ?)',
                 (question['id'], question['answer'], question['question'], question['value']))
 
     conn.commit()
 
-categories = ['49', '253', '832', '1723', '15878']
 
-for cat in categories:
-        get_question_and_save_into_db(cat)
+def random_value():
+    return 100 * random.randint(1,3)
+
+
